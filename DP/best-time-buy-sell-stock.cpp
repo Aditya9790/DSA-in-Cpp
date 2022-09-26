685e4ccf1a030e9dfa22c742606bc72c1817db0d
Aditya Gole <57624307+Aditya9790@users.noreply.github.com>
2022-09-27 01:15:52 +0530

    Update best-time-buy-sell-stock.cpp

--- DP/best-time-buy-sell-stock.cpp
+++ DP/best-time-buy-sell-stock.cpp
@@ -3,7 +3,7 @@
         int maxprofit = 0, mini = prices[0];
         for(int i=1;i<prices.size();i++)
         {
-            int cost = prices[i] - mini; // profit is sold at current day
+            int cost = prices[i] - mini; // profit if sold at current day
             maxprofit = max(maxprofit, cost); // max profit
             mini = min(mini, prices[i]); // min cost encountered until i th index
             
